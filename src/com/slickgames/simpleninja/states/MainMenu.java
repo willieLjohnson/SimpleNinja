f68c996408346d78e3c1b5c079d07fdf78fbfa2e
SlickJohnson <liwa.johnson@gmail.com>
2015-12-18 17:27:06 -0500

    Updated mainmenu

--- src/com/slickgames/simpleninja/states/MainMenu.java
+++ src/com/slickgames/simpleninja/states/MainMenu.java
@@ -3,12 +3,11 @@ package com.slickgames.simpleninja.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -17,8 +16,6 @@ import com.badlogic.gdx.utils.Align;
 import com.slickgames.simpleninja.handlers.Animation;
 import com.slickgames.simpleninja.handlers.GameStateManager;
 import com.slickgames.simpleninja.main.Game;
-
-import static com.slickgames.simpleninja.handlers.B2DVars.PPM;
 
 /**
  * Created by Administrator on 12/8/2015.
@@ -33,7 +30,7 @@ public class MainMenu extends GameState {
     private TextButton quitButton;
 
     private SpriteBatch batch;
-    private Sprite sprite;
+    private Sprite waterfall;
     Animation animation;
     TextureRegion[] MainMenu1S;
     int mainmenu = 1;
@@ -44,12 +41,15 @@ public class MainMenu extends GameState {
 
         //stage/////////////////////////
 //        stage = new Stage(new FitViewport(Game.V_WIDTH,Game.V_HEIGHT));
-        skin = new Skin(Gdx.files.internal("res/maps/uiskin.json"));
+        skin = new Skin(Gdx.files.internal("res/maps/uiskin.json") , new TextureAtlas(Gdx.files.internal("res/maps/uiskin.atlas")));
 
+        BitmapFont bt = new BitmapFont();
         table = new Table();
         table.setFillParent(true);
         table.setWidth(game.stage.getWidth());
-        table.align(Align.right);
+        table.align(Align.center);
+        Label.LabelStyle ls = new Label.LabelStyle(bt, Color.WHITE);
+        Label label = new Label("Simple Ninja 1.0", ls);
         startButton = new TextButton("Start", skin);
         OptionsButton = new TextButton("Option", skin);
         quitButton = new TextButton("Quit", skin);
@@ -62,7 +62,8 @@ public class MainMenu extends GameState {
         });
 
         table.padTop(30);
-
+        table.add(label);
+        table.row();
         table.add(startButton);
         table.row();
         table.add(OptionsButton);
@@ -75,9 +76,12 @@ public class MainMenu extends GameState {
         animation = new Animation();
         if (mainmenu == 1) {
             MainMusic = Gdx.audio.newMusic(Gdx.files.internal("res/music/MainMusic.mp3"));
-            Texture Mainmenu1 = Game.game.getAssetManager().get("res/maps/Main1.Png");
-            MainMenu1S = TextureRegion.split(Mainmenu1, 500, 475)[0];
+
+            MainMenu1S = TextureRegion.split(Game.game.getAssetManager().get("res/maps/Main1.Png"), 500, 475)[0];
             animation.setFrames(MainMenu1S, 1 / 25f);
+            waterfall = new Sprite(animation.getFrame());
+            waterfall.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
         }
 // if (mainmenu==1){
 //            Texture Mainmenu1 = Game.game.getAssetManager().get("res/maps/test two.png");
@@ -100,7 +104,8 @@ public class MainMenu extends GameState {
     @Override
     public void update(float dt) {
         animation.update(dt);
-
+        waterfall = new Sprite(animation.getFrame());
+        waterfall.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
@@ -108,15 +113,14 @@ public class MainMenu extends GameState {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         sb.begin();
-        sb.draw(animation.getFrame(), 0, 0);
+        waterfall.draw(sb);
         sb.end();
         gsm.game().stage.act(Gdx.graphics.getDeltaTime());
         gsm.game().stage.draw();
-
-
     }
 
 
+    @Override
     public void dispose() {
         game.stage.dispose();
         MainMusic.dispose();
